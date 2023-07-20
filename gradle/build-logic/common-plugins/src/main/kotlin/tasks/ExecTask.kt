dc2e09ffe2454c9ecb02b431f0a835896a40bdc4
Suresh G <sureshg.g@gmail.com>
2023-07-21 00:17:21 +0530

    chore: add task samples

---
+++ gradle/build-logic/common-plugins/src/main/kotlin/tasks/ExecTask.kt
@@ -0,0 +1,36 @@
+package tasks
+
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.*
+
+/**
+ * A sample [JavaExec] task that executes class from runtime classpath is as follows:
+ * ```kotlin
+ * val execTask by registering(JavaExec::class) {
+ *         doFirst { ... }
+ *         group = "build"
+ *         mainClass = "app.Main"
+ *         classpath = sourceSets.main.get().runtimeClasspath
+ *         args = listOf(....)
+ *         outputs.dir(outDir)
+ *         doLast { }
+ *         finalizedBy(copyTask)
+ * }
+ * ```
+ */
+@CacheableTask
+abstract class ExecTask : JavaExec() {
+
+  @get:InputFile
+  @get:PathSensitive(PathSensitivity.RELATIVE)
+  abstract val sourceFile: RegularFileProperty
+
+  @get:OutputDirectory abstract val outputDir: DirectoryProperty
+
+  init {
+    group = "build"
+    mainClass.set("app.Main")
+    argumentProviders.add { listOf(outputDir.get().asFile.path, sourceFile.get().asFile.path) }
+  }
+}
