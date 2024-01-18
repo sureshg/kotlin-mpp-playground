585689c00c8f3bc4be166b0177cccdfc90b394cc
Suresh G <sureshg.g@gmail.com>
2024-01-17 17:11:22 -0800

    chore: dep updates

--- shared/src/jvmMain/java/dev/suresh/Lang.java
+++ shared/src/jvmMain/java/dev/suresh/Lang.java
@@ -2,3 +2,43 @@ package dev.suresh;
 
 public record Lang(String name) {
 }
+
+
+record Model(String name, float temp, int tokens) {
+
+    public static ModelBuilder builder() {
+        return new ModelBuilder();
+    }
+
+    void main() {
+
+        var b = Model.builder().name("test").build();
+
+    }
+}
+
+class ModelBuilder {
+    private String name;
+    private float temp = 0.1f;
+
+    private int tokens = 100;
+
+    public ModelBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ModelBuilder temp(float temp) {
+        this.temp = temp;
+        return this;
+    }
+
+    public ModelBuilder tokens(int tokens) {
+        this.tokens = tokens;
+        return this;
+    }
+
+    public Model build() {
+        return new Model(name, temp, tokens);
+    }
+}
