194bb19fc374a0b5b518be6c698b16f38c83d33b
Suresh G <sureshg.g@gmail.com>
2024-03-18 09:53:43 -0700

    chore: dep updates

--- gradle/build-logic/src/main/kotlin/plugins/graalvm.gradle.kts
+++ gradle/build-logic/src/main/kotlin/plugins/graalvm.gradle.kts
@@ -40,8 +40,10 @@ graalvmNative {
       richOutput = true
       buildArgs = buildList {
         add("--enable-preview")
+        add("--enable-native-access=ALL-UNNAMED")
         add("--native-image-info")
-        add("--enable-monitoring=heapdump,jfr,jvmstat")
+        add("--color=auto")
+        add("--enable-monitoring=heapdump,jfr,jvmstat,threaddump,nmt")
         add("--enable-https")
         add("--install-exit-handlers")
         add("--features=graal.aot.RuntimeFeature")
