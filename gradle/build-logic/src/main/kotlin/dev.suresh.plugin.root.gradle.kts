fe55e6a308a50dca9b2eb68efbe29fcb90cc4a89
Suresh G <sureshg.g@gmail.com>
2025-03-03 15:13:27 -0800

    chore: dep updates

--- gradle/build-logic/src/main/kotlin/dev.suresh.plugin.root.gradle.kts
+++ gradle/build-logic/src/main/kotlin/dev.suresh.plugin.root.gradle.kts
@@ -34,8 +34,8 @@ gradle.projectsEvaluated { logger.lifecycle(magenta("=== Projects Configuration 
 
 idea {
   module {
-    isDownloadJavadoc = true
-    isDownloadSources = true
+    isDownloadJavadoc = false
+    isDownloadSources = false
   }
   project.vcs = "Git"
 }
