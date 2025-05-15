980140b64dae0c8bf5f7ffe4bbf754f2674b6d16
Suresh G <sureshg.g@gmail.com>
2025-05-15 09:16:05 -0700

    chore: misc fixes

--- backend/data/src/main/kotlin/dev/suresh/DataApp.kt
+++ backend/data/src/main/kotlin/dev/suresh/DataApp.kt
@@ -1,20 +1,10 @@
 package dev.suresh
 
-import com.ionspin.kotlin.bignum.integer.BigInteger
 import com.pty4j.PtyProcessBuilder
 import java.util.concurrent.CountDownLatch
 
 fun main() {
   println("Hello Kotlin Data! ${Greeting().greeting()}")
-
-  val b1 = BigInteger.parseString("12345678901234567890", 10)
-  val b2 = BigInteger.parseString("323456789012345678901", 10)
-  val s = b1 + b2
-
-  println(b1.bitLength())
-  println(b2.bitLength())
-  println(s)
-  println(s.bitLength())
   ssh()
 }
 
