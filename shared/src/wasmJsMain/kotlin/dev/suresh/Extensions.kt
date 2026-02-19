1e42cd1e0f8b078611661226dd12cba0b58eb995
Suresh G <sureshg.g@gmail.com>
2026-02-19 00:02:55 -0800

    chore: dep updates and simplify ArrayBuffer extension using built-in toByteArray

--- shared/src/wasmJsMain/kotlin/dev/suresh/Extensions.kt
+++ shared/src/wasmJsMain/kotlin/dev/suresh/Extensions.kt
@@ -1,9 +1,8 @@
 package dev.suresh
 
-import kotlin.wasm.unsafe.UnsafeWasmMemoryApi
-import kotlin.wasm.unsafe.withScopedMemoryAllocator
 import org.khronos.webgl.ArrayBuffer
 import org.khronos.webgl.Int8Array
+import org.khronos.webgl.toByteArray
 
 /** JavaScript console class */
 external class Console : JsAny {
@@ -19,29 +18,4 @@ fun currentTimeMillis(): Long = currentTimeMillisJs().toLong()
 
 private fun currentTimeMillisJs(): Double = js("new Date().getTime()")
 
-fun ArrayBuffer?.toByteArray() =
-    this?.run {
-      val source = Int8Array(this, 0, byteLength)
-      jsInt8ArrayToKotlinByteArray(source)
-    }
-
-@JsFun(
-    """ (src, size, dstAddr) => {
-          const mem8 = new Int8Array(wasmExports.memory.buffer, dstAddr, size);
-          mem8.set(src);
-         }
-         """
-)
-internal external fun jsExportInt8ArrayToWasm(src: Int8Array, size: Int, dstAddr: Int)
-
-internal fun jsInt8ArrayToKotlinByteArray(x: Int8Array): ByteArray {
-  val size = x.length
-
-  @OptIn(UnsafeWasmMemoryApi::class)
-  return withScopedMemoryAllocator { allocator ->
-    val memBuffer = allocator.allocate(size)
-    val dstAddress = memBuffer.address.toInt()
-    jsExportInt8ArrayToWasm(x, size, dstAddress)
-    ByteArray(size) { i -> (memBuffer + i).loadByte() }
-  }
-}
+fun ArrayBuffer?.toByteArray() = this?.run { Int8Array(this).toByteArray() }
